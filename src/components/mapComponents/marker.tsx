77bdeaf6100aee2bc11d6a9ab65508968ec08007
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2021-07-29 00:07:56 +0900

    Revert "delete marker.tsx"
    This reverts commit 32f970fde667dfe02f09e54a094ddea262cfffed.

---
+++ src/components/mapComponents/marker.tsx
@@ -0,0 +1,47 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import {
+  Marker, Popup,
+} from 'react-leaflet'
+import L from 'leaflet'
+
+import { RamenShop } from '../../interfaces'
+
+const MarkerComponent = (
+  { item, icon, category } :
+  {
+  item: RamenShop,
+  icon: L.Icon
+  category: string
+},
+): JSX.Element => {
+  return (
+    <Marker
+      position={[item.lat, item.lng]}
+      key={item.id}
+      icon={icon}
+    >
+      <Popup>
+        <p style={{ maxWidth: '100px', fontWeight: 'bolder' }}>
+          <a href={item.url} target="_blank" rel="noreferrer">
+            {item.name}
+          </a>
+        </p>
+        <p>
+          {`${category}/${item.area.toUpperCase()}`}
+        </p>
+        <p style={{ maxWidth: '100px', fontSize: 'smaller' }}>
+          <LocationOnIcon fontSize="small" />
+          <a href={`http://maps.google.co.jp/maps?q=${item.address}`} target="_blank" rel="noreferrer">
+            {item.address}
+          </a>
+        </p>
+
+      </Popup>
+    </Marker>
+
+  )
+}
+
+export default MarkerComponent
