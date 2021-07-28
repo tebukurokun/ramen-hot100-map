957c1e44fde1a777e01a8d62005add60fb181b76
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2021-07-29 00:15:11 +0900

    Revert "tmp"
    This reverts commit 6488678f0a24c251d274d6cabc430b54850cd380.

--- next.config.js
+++ next.config.js
@@ -8,11 +8,4 @@ module.exports = {
     )
     return config
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 }
