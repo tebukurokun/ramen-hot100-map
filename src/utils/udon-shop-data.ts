abf3ec2479b5d95a1c19753badacab1b6cdc0047
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2022-01-05 01:18:12 +0900

    refactor udon data

--- src/utils/udon-shop-data.ts
+++ src/utils/udon-shop-data.ts
@@ -1,64 +1,14 @@
 import { Shop } from '../interfaces'
-import * as tokyoUdonData from './datafiles/udon-tokyo.json'
-import * as eastUdonData from './datafiles/udon-east.json'
-import * as westUdonData from './datafiles/udon-west.json'
-
-/**
- * jsonファイルから取得したtokyoデータ
- *
- * example: {id: '1', name: 'Alice', url: 'https://tabelog.com/tokyo/A1314/A131402/13006051/', address: '東京都港区三田2-16-4', lng: 139.74152, lat: 35.64804,}
- *
- */
-const udonShopsDataToyko: Shop[] = tokyoUdonData.shops.map(
-  ( shop ) => {
-    return {
-      id: `tokyo-${shop.id}`,
-      name: shop.name,
-      area: 'tokyo',
-      url: shop.url,
-      code: shop.code,
-      // imgPath: `/static/images/udon/${shop.code}.jpg`,
-      imgPath: '',
-      address: shop.address,
-      lng: parseFloat( shop.fX ),
-      lat: parseFloat( shop.fY ),
-    }
-  },
-)
+import { shops } from './datafiles/udon.json'
 
 /**
  * jsonファイルから取得したeastデータ
  *
- * example: {id: '1', name: 'Alice', url: 'https://tabelog.com/tokyo/A1314/A131402/13006051/', address: '東京都港区三田2-16-4', lng: 139.74152, lat: 35.64804,}
- *
  */
-const udonShopsDataEast: Shop[] = eastUdonData.shops.map(
+const udonShopsData: Shop[] = shops.map(
   ( shop ) => {
     return {
-      id: `east-${shop.id}`,
-      name: shop.name,
-      area: 'east',
-      url: shop.url,
-      code: shop.code,
-      // imgPath: `/static/images/udon/${shop.code}.jpg`,
-      imgPath: '',
-      address: shop.address,
-      lng: parseFloat( shop.fX ),
-      lat: parseFloat( shop.fY ),
-    }
-  },
-)
-
-/**
- * jsonファイルから取得したeastデータ
- *
- * example: {id: '1', name: 'Alice', url: 'https://tabelog.com/tokyo/A1314/A131402/13006051/', address: '東京都港区三田2-16-4', lng: 139.74152, lat: 35.64804,}
- *
- */
-const udonShopsDataWest: Shop[] = westUdonData.shops.map(
-  ( shop ) => {
-    return {
-      id: `west-${shop.id}`,
+      id: shop.id,
       name: shop.name,
       area: 'west',
       url: shop.url,
@@ -66,16 +16,12 @@ const udonShopsDataWest: Shop[] = westUdonData.shops.map(
       // imgPath: `/static/images/udon/${shop.code}.jpg`,
       imgPath: '',
       address: shop.address,
-      lng: parseFloat( shop.fX ),
-      lat: parseFloat( shop.fY ),
+      lng: parseFloat( shop.lng ),
+      lat: parseFloat( shop.lat ),
     }
   },
 )
 
-const udonShopsData: Shop[] = udonShopsDataToyko
-  .concat( udonShopsDataEast )
-  .concat( udonShopsDataWest )
-
 export {
-  udonShopsDataToyko, udonShopsDataEast, udonShopsDataWest, udonShopsData,
+  udonShopsData,
 }
