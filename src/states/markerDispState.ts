ad09b7cae66383f09beba42bab45aa23aaa949bd
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2022-01-04 14:51:18 +0900

    refactor

--- src/states/markerDispState.ts
+++ src/states/markerDispState.ts
@@ -1,8 +1,9 @@
 import { atom } from 'recoil'
 
 import { MarkerDispState } from '../interfaces/MarkerDispState'
+import { RecoilAtomKeys } from './RecoilKeys'
 
 export const stateMarkerDisp = atom<MarkerDispState>( {
-  key: 'state-marker-disp',
+  key: RecoilAtomKeys.MARKER_DISP_STATE,
   default: { ramen: true, udon: true, curry: true },
 } )
