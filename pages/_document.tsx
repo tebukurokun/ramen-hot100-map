600190ba428fad91bb726aa5647e839d9d684212
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2024-12-06 00:03:03 +0900

    delete

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,25 +15,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
-
-// MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-//   const sheets = new ServerStyleSheets();
-//   const originalRenderPage = ctx.renderPage;
-
-//   ctx.renderPage = () =>
-//     originalRenderPage({
-//       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-//     });
-
-//   const initialProps = await Document.getInitialProps(ctx);
-
-//   return {
-//     ...initialProps,
-//     styles: (
-//       <>
-//         {initialProps.styles}
-//         {sheets.getStyleElement()}
-//       </>
-//     ),
-//   };
-// };
