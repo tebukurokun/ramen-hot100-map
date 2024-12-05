516a3640ec7a454cc6e89e481cd465a55f9138a8
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2024-12-05 18:15:25 +0900

    fix app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,21 @@ import { Analytics } from "@vercel/analytics/next";
 import { Provider } from "jotai";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { useEffect } from "react";
 import "../styles/main.css";
 
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  useEffect(() => {
-    const jssStyles: Element | null =
-      document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <Provider>
       <Head>
         <title>Hyakumeiten Map</title>
-        <link rel="shortcut icon" href="static/favicon/favicon.ico" />
+        <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
           content="ラーメン百名店の情報をマップで見ることができるサイトです。"
         />
+        <meta property="og:title" content="Hyakumeiten Map" />
         <meta
           property="og:description"
           content="ラーメン百名店の情報をマップで見ることができるサイトです。"
