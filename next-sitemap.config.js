870f89f18df40fef9333bbd41ce8b6a4cfdff6dc
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2024-12-06 16:24:46 +0900

    add meta

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://hyakumeiten-map.vercel.app/',
+  generateRobotsTxt: true, // robots.txtを生成
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,11 @@ const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
           name="description"
           content="ラーメン百名店の情報をマップで見ることができるサイトです。"
         />
+        <meta
+          name="keywords"
+          content="ラーメン, 地図, マップ, 百名店, ラーメン百名店, 百名店マップ"
+        />
+        <meta name="author" content="tebukuro" />
         <meta property="og:title" content="Hyakumeiten Map" />
         <meta
           property="og:description"
@@ -24,6 +29,7 @@ const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
         <meta property="og:site_name" content="Hyakumeiten Map" />
         <meta property="og:url" content="https://hyakumeiten-map.vercel.app/" />
         <meta property="og:image" content="/static/preview.jpg" />
+        <meta property="og:type" content="website" />
         <link rel="preload" href="/static/marker-icons/ramen.png" as="image" />
       </Head>
       <Component {...pageProps} />
