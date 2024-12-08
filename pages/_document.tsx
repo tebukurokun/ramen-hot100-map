82ab408c52d183e8a75df8bcfb1eb19f93744e9a
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2024-12-08 14:08:04 +0900

    add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,33 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ja">
-        <Head></Head>
+        <Head>
+          {/* GAのメインスクリプト読込 */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-SZEKVQ44B5"
+            strategy="afterInteractive"
+          />
+
+          {/* GAの初期化スクリプト挿入 */}
+          <Script
+            id="ga-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-SZEKVQ44B5', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
